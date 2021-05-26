a2da3b5f0ef7a69e5f67f50beadde00146b33b4e
juritomak <juri.tomak@gmail.com>
2021-05-26 15:47:46 +0200

    implemented a loadshape for real-world workload patterns; at the moment is uses requests per hour of a recorded workload and spawns locust users accordingly

--- locust_scripts/locust_teastore.py
+++ locust_scripts/locust_teastore.py
@@ -7,10 +7,12 @@
 import logging
 import random
 import re
-from datetime import timedelta
+from re import search
+from datetime import timedelta, datetime
+from glob import glob
 
 import requests
-from locust import between, task, HttpUser, events, LoadTestShape
+from locust import between, task, HttpUser, events, LoadTestShape, constant
 from locust.contrib.fasthttp import FastHttpUser
 from locust.env import Environment
 
@@ -29,41 +31,92 @@ def my_success_handler(request_type, name, response_time, response_length, **kw)
     logging.info("Response time %s ms", response_time)
 
 
-class StagesShape(LoadTestShape):
-    """
-    A simple load test shape class that has different user and spawn_rate at
-    different stages.
-    Keyword arguments:
-        stages -- A list of dicts, each representing a stage with the following keys:
-            duration -- When this many seconds pass the test is advanced to the next stage
-            users -- Total user count
-            spawn_rate -- Number of users to start/stop per second
-            stop -- A boolean that can stop that test at a specific stage
-    """
+def contains_timestamp_with_ms(line: str):
+    return search(r"\s*\d*-\d*-\d*\s\d*:\d*:\d*\.\d*", line) is not None
 
-    stages = [
-        {"duration": timedelta(seconds=60).total_seconds(), "users": 1, "spawn_rate": 1},
-        {"duration": timedelta(seconds=120).total_seconds(), "users": 10, "spawn_rate": 1},
-        {"duration": timedelta(seconds=240).total_seconds(), "users": 50, "spawn_rate": 10},
-        {"duration": timedelta(seconds=480).total_seconds(), "users": 20, "spawn_rate": 1},
-    ]
+
+def get_timestamp_from_string(line: str):
+    return search(r"\s*\d*-\d*-\d*\s\d*:\d*:\d*\.?\d*", line).group().strip()
+
+
+def get_timestamp_from_line(line: str) -> datetime:
+    if contains_timestamp_with_ms(line):
+        format_string = '%Y-%m-%d %H:%M:%S.%f'
+    else:
+        format_string = '%Y-%m-%d %H:%M:%S'
+
+    return datetime.strptime(
+        get_timestamp_from_string(line),
+        format_string
+    )
+
+
+class RealWorkloadShape(LoadTestShape):
+    def __init__(self):
+        super().__init__()
+
+        self._workload_pattern = dict()
+
+        for file_path in sorted(glob("GS Production Workload/*.log")):
+            with open(file_path) as logfile:
+                for line in logfile:
+                    requests_per_hour = re.search('(?<=RPH:\\s)\\d*', line)
+                    if requests_per_hour is None:
+                        continue
+
+                    time_stamp = get_timestamp_from_line(line)
+
+                    requests_per_hour = int(requests_per_hour.group())
+
+                    self._workload_pattern[time_stamp.hour] = requests_per_hour
 
     def tick(self):
         run_time = self.get_run_time()
 
-        for stage in self.stages:
-            if run_time < stage["duration"]:
-                tick_data = (stage["users"], stage["spawn_rate"])
-                return tick_data
+        run_time_in_hours = int(run_time / 3600)
 
-        self.reset_time()
-        last_stage = self.stages[len(self.stages) - 1]
-        return last_stage["users"], last_stage["spawn_rate"]
+        avg_requests_per_second = int(self._workload_pattern[run_time_in_hours] / 3600)
+
+        # Because every user sends one request per second, we need avg_requests_per_second users
+        return avg_requests_per_second, avg_requests_per_second
+
+
+# class StagesShape(LoadTestShape):
+#     """
+#     A simple load test shape class that has different user and spawn_rate at
+#     different stages.
+#     Keyword arguments:
+#         stages -- A list of dicts, each representing a stage with the following keys:
+#             duration -- When this many seconds pass the test is advanced to the next stage
+#             users -- Total user count
+#             spawn_rate -- Number of users to start/stop per second
+#             stop -- A boolean that can stop that test at a specific stage
+#     """
+#
+#     stages = [
+#         {"duration": timedelta(seconds=60).total_seconds(), "users": 1, "spawn_rate": 1},
+#         {"duration": timedelta(seconds=120).total_seconds(), "users": 10, "spawn_rate": 1},
+#         {"duration": timedelta(seconds=240).total_seconds(), "users": 50, "spawn_rate": 10},
+#         {"duration": timedelta(seconds=480).total_seconds(), "users": 20, "spawn_rate": 1},
+#     ]
+#
+#     def tick(self):
+#         run_time = self.get_run_time()
+#
+#         for stage in self.stages:
+#             if run_time < stage["duration"]:
+#                 tick_data = (stage["users"], stage["spawn_rate"])
+#                 return tick_data
+#
+#         self.reset_time()
+#         last_stage = self.stages[len(self.stages) - 1]
+#         return last_stage["users"], last_stage["spawn_rate"]
 
 
 # class TeaStore(HttpUser):
 class TeaStore(FastHttpUser):
-    wait_time = between(8, 8)
+    # wait_time = between(8, 8)
+    wait_time = constant(1)
 
     _global_user_count = 0
 
