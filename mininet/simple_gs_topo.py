682b0d1e973846440bed359b41e45007b94a679b
JTO <juri.tomak@gselectronic.com>
2022-04-08 16:33:47 +0000

    Added linkoptions as measured in the production system Also added simplified topologies for testing purposes

--- mininet/simple_gs_topo.py
+++ mininet/simple_gs_topo.py
@@ -1,56 +1,64 @@
-"""GS topologies
-
-SimpleTopo:
-
-  host (AS) 
-    --- switch (rounter at customer's home) --- (internet connection cust) 
-    --- switch(internet connection AP) --- switch (router at ARC) 
-  --- host (ARC)
-
-"""
-
 from mininet.topo import Topo
 from mininet.cli import CLI
 
+import time
+
 class SimpleTopo( Topo ):
+    """
+        host (AS)
+        --- switch (rounter at customer's home) --- (ISP router cust)
+        --- switch(ISP router AP) --- switch (router at ARC)
+        --- host (ARC)
+    """
+
     def build( self ):
 
         # Add hosts and switches
         alarm_system = self.addHost( 'h_as' )
         alarm_receiving_centre = self.addHost( 'h_arc' )
         
-        leftSwitch = self.addSwitch( 's1' )
-        ispSwitch = self.addSwitch( 's2' )
-        rightSwitch = self.addSwitch( 's3' )
+        customerSwitch = self.addSwitch( 's1' )
+        ispCustomerSwitch = self.addSwitch( 's2' )
+        ispAPSwitch = self.addSwitch( 's3' )
+        apSwitch = self.addSwitch( 's4' )
 
         # Add links
 
-        # Oversimplified links for testing purposes
-        linkopts = {'delay':'1s' }
-        self.addLink( alarm_system, leftSwitch, **linkopts )
-        linkopts = {'delay':'2s' }
-        self.addLink( leftSwitch, rightSwitch, **linkopts )
-        linkopts = {'delay':'1s' }
-        self.addLink( rightSwitch, alarm_receiving_centre, **linkopts )
+        # -- Test Oversimplified links for testing purposes --
+        #linkopts = {'delay':'10ms' }
+        #self.addLink( alarm_system, customerSwitch, **linkopts )
+        #linkopts = {'delay':'0.5s' }
+        #self.addLink( customerSwitch, apSwitch, **linkopts )
+        #linkopts = {'delay':'10ms' }
+        #self.addLink( apSwitch, alarm_receiving_centre, **linkopts )
+        
+        self.addLink( alarm_system, customerSwitch) 
+        self.addLink( customerSwitch, apSwitch)
+        self.addLink( apSwitch, alarm_receiving_centre)
 
         return
+        # --
         
         # Simulate production system
         # customer has 1 Gigabit Ethernet (GbE) connection to his router 
-        linkopts = {'bw':1000, 'delay':'1ms' }
-        self.addLink( alarm_system, leftSwitch, **linkopts )
+        linkopts = {'bw':1000, 'delay':'0.45ms' }
+        self.addLink( alarm_system, customerSwitch, **linkopts )
 
-        # customer has VDSL 100 (100 MBit Download, 40 MBit Upload) 
-        linkopts = {'bw':40, 'delay':'1ms' }
-        self.addLink( leftSwitch, ispSwitch, **linkopts )
+        # customer has VDSL 100 (100 MBit Download, 30 MBit Upload) 
+        linkopts = {'bw':30, 'delay':'2.4ms', 'jitter': '5.2ms' }
+        self.addLink( customerSwitch, ispCustomerSwitch, **linkopts )
         
-        # alarm provider has SDSL 100 (100 MBit Down- /Upload)
-        linkopts = {'bw':100, 'delay':'1ms' }
-        self.addLink( ispSwitch, rightSwitch, **linkopts )
+        # ISP Interlink (10 GbE connection) 
+        linkopts = {'bw':10000, 'delay':'13.3ms', 'jitter': '3.15ms' }
+        self.addLink( ispCustomerSwitch, ispAPSwitch, **linkopts )
+        
+        # alarm provider sadly does NOT have SDSL, rather LACP-based Uplinks: VDSL100, VDSL50 combined
+        linkopts = {'bw':50, 'delay':'7.97ms', 'jitter': '2.9ms' }
+        self.addLink( ispAPSwitch, apSwitch, **linkopts )
 
-        # alarm provider has 1 GbE connection to his router
-        linkopts = {'bw':1000, 'delay':'0.3ms', 'loss':16 }
-        self.addLink( rightSwitch, alarm_receiving_centre, **linkopts )
+        # alarm provider has 2 GbE connection to his router
+        linkopts = {'bw':2000, 'delay':'0.19ms', 'jitter': '0.06ms' }
+        self.addLink( apSwitch, alarm_receiving_centre, **linkopts )
 
 
 python_configured_hosts = []
@@ -132,7 +140,7 @@ def start_alarm_system_workload(net):
     setup_python_on_host(h_as)
 
     cmd = 'python locust-parameter-variation.py locust/gen_gs_alarm_device_workload.py -u http://{}:1337 -p'.format(arc.IP())
-    print("AS: Starting Production Workload ...")
+    print("AS: Starting Alarm Device Workload ...")
     print(cmd)
 
     cmd_output = h_as.cmd("{} &> /dev/null &".format(cmd))
@@ -150,7 +158,10 @@ def stop_workloads(self, args):
 def LocustTest(net):
     
     start_ARS(net)
-    # start_production_workload(net)
+    time.sleep(2)
+    start_production_workload(net)
+    time.sleep(1)
+    start_alarm_system_workload(net)
 
     # net.start()
     CLI(net)
