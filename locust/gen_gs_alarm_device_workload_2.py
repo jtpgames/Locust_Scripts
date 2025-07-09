3a564c43b127b5e18c776ac846546f5eae543504
Juri Tomak <juritomak@Mac.fritz.box>
2025-07-09 19:10:37 +0200

    feat(gen_gs_alarm_device_workload_2): implement custom run_time management that allows each client to finish its pending request before shutting down the test.

--- locust/gen_gs_alarm_device_workload_2.py
+++ locust/gen_gs_alarm_device_workload_2.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import random
+import os
+import gevent
+import logging
 
-from locust import task, between, User, constant
+from locust import task, between, User, constant, events
+from locust.env import Environment
 
 from common.common_locust import RepeatingHttpxClient
 
-from locust import User, task, between
-
 import random
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 class RepeatingHttpLocust(User):
     abstract = True
@@ -20,6 +22,23 @@ class RepeatingHttpLocust(User):
 
 # initialize the random seed value to get reproducible random sequences
 random.seed(42)
+
+EXPERIMENT_RUNTIME = timedelta(minutes=float(os.environ.get('EXPERIMENT_RUNTIME', 10)))
+
+locust_environment: Environment = None
+experiment_starttime: datetime = datetime.now()
+
+@events.init.add_listener
+def on_test_start(environment: Environment, **kwargs):
+    environment.stop_timeout = 10
+
+    global locust_environment
+    locust_environment = environment
+
+    global experiment_starttime
+    experiment_starttime = datetime.now()
+    LOGGER = logging.getLogger('AlarmDevice')
+    LOGGER.info(f"Experiment started @{experiment_starttime} to run for {EXPERIMENT_RUNTIME}")
 
 
 class AlarmDevice(RepeatingHttpLocust):
@@ -36,9 +55,13 @@ class AlarmDevice(RepeatingHttpLocust):
     available_phone_numbers_for_devices = ["015142611148", "01754937448", "016590943333"]
     available_branch_numbers_for_devices = list(range(2001, 2011)) + list(range(2012, 2017))
     
+    currently_executing_users = 0
+    LOGGER = logging.getLogger('AlarmDevice')
+
     def __init__(self, *args, **kwargs):
         super(AlarmDevice, self).__init__(*args, **kwargs)
         self.random_phone = random.choice(self.available_phone_numbers_for_devices)
+        AlarmDevice.currently_executing_users += 1
     
     def construct_call_with_reject_request_object(self):
         random_branch = random.choice(self.available_branch_numbers_for_devices)
@@ -59,6 +82,17 @@ class AlarmDevice(RepeatingHttpLocust):
         json_obj = self.construct_call_with_reject_request_object()
 
         response = self.client.send("/api/v1/simple", json_obj)
+
+        now = datetime.now()
+        time_since_experiment = now - experiment_starttime
+        if time_since_experiment > EXPERIMENT_RUNTIME:
+            AlarmDevice.LOGGER.info(f"Stopping user")
+            if AlarmDevice.currently_executing_users == 1:
+                AlarmDevice.LOGGER.info(f"Stopping Runner")
+                gevent.spawn_later(2, locust_environment.runner.quit)
+            self.stop()
+            AlarmDevice.currently_executing_users -= 1
+
 
     @task(1)
     def call_with_reject(self):
